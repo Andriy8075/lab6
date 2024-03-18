674d40c314ee5a72c20b148e6f97d96778b500cd
andriy8075 <andriy8075@gmail.com>
2024-03-18 17:51:24 +0200

    corrected

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 const radius = 200;
-const lineWidth = 3;
-const size = 10
+const lineWidth = 1;
+const arrowWidth = 5;
+const arrowLengthInLine = 16;
+const arrowLengthInElipse = 16;
+const countOfVertixes = 10;
 const vertixRadius = 18;
+const distanceBetweenLines = 10;
+const textAreaFontSize = 20;
+const k = 0.745;
 
 const canvas = document.createElement('canvas');
 canvas.style.position = 'absolute';
@@ -17,12 +23,11 @@ const textAreas = [document.createElement('textarea'),
 for(let i = 0; i < textAreas.length; i++) {
     const textArea = textAreas[i]
     textArea.style.position = 'absolute';
-    textArea.rows = size+1;
-    textArea.cols = 23;
+    textArea.rows = countOfVertixes+1;
+    textArea.cols = countOfVertixes*3-6;
     textArea.style.top = `${190 + radius * 2}px`;
     textArea.style.left = `${window.innerWidth/(i ? 4/3 : 4) - textArea.cols*5}px`;
-    textArea.style.fontSize = '20px';
-    textArea.id = 'textArea1';
+    textArea.style.fontSize = `${textAreaFontSize}px`;
 }
 const createVertixes = (count, radius, left) => {
     const vertixes = [];
@@ -35,10 +40,10 @@ const createVertixes = (count, radius, left) => {
         vertix.innerHTML = (i+1).toString();
         vertix.style.border= '3px solid #000';
         vertix.style.textAlign = 'center';
-        vertix.style.borderRadius = '50%';
+        vertix.style.borderRadius = '100%';
         vertix.style.position = 'absolute';
-        vertix.style.width = '1em';
-        vertix.style.height = '1em';
+        vertix.style.width = '32px';
+        vertix.style.height = '32px';
         vertix.style.left = `${x}px`;
         vertix.style.top = `${y}px`;
         document.body.appendChild(vertix);
@@ -51,28 +56,35 @@ const createVertixes = (count, radius, left) => {
 const createLine = (x1, y1, x2, y2, arrow) => {
     const dx = x1 - x2;
     const dy= y1 - y2;
-    const dLength = Math.sqrt(dx*dx + dy*dy);
-    const xShorter = vertixRadius*dx/dLength;
-    const yShorter = vertixRadius*dy/dLength;
+    const lineLength = Math.sqrt(dx*dx + dy*dy);
+    const lineCos = dx/lineLength;
+    const lineSin = dy/lineLength;
     ctx.strokeStyle = 'black';
     ctx.lineWidth = lineWidth;
-    ctx.moveTo(x1-xShorter, y1-yShorter);
     if(!arrow) {
-        ctx.lineTo(x2+xShorter, y2+yShorter);
+        const xForwardShift = vertixRadius*lineCos;
+        const yForwardShift = vertixRadius*lineSin;
+        ctx.moveTo(x1-xForwardShift, y1-yForwardShift);
+        ctx.lineTo(x2+xForwardShift, y2+yForwardShift);
         ctx.stroke();
     }
     else {
-        let lastPositionX = x2+xShorter+20*dx/dLength;
-        let lastPositionY = y2+yShorter+20*dy/dLength;
+        const xForwardShift = (vertixRadius*Math.cos(Math.asin(distanceBetweenLines/vertixRadius)))*lineCos;
+        const yForwardShift = (vertixRadius*Math.sin(Math.acos(distanceBetweenLines/vertixRadius)))*lineSin;
+        const xSideShift = distanceBetweenLines*lineSin;
+        const ySideShift = distanceBetweenLines*lineCos;
+        let lastPositionX = x2+xForwardShift+arrowLengthInLine*lineCos+xSideShift;
+        let lastPositionY= y2+yForwardShift+arrowLengthInLine*lineSin-ySideShift;
+        ctx.moveTo(x1-xForwardShift+xSideShift, y1-yForwardShift-ySideShift);
         ctx.lineTo(lastPositionX, lastPositionY);
         ctx.stroke();
-        for(let i = 0; lineWidth*3-i > 0; i++) {
+        for(let i = arrowWidth; i > 0; i--) {
             ctx.beginPath();
             ctx.moveTo(lastPositionX, lastPositionY);
-            ctx.lineWidth = lineWidth*3-i;
-            lastPositionX = x2+xShorter+(20*(lineWidth*3 - i)/(lineWidth*3))*dx/dLength;
-            lastPositionY = y2+yShorter+(20*(lineWidth*3 - i)/(lineWidth*3))*dy/dLength;
-            ctx.lineTo(lastPositionX, lastPositionY)
+            ctx.lineWidth = i;
+            lastPositionX = x2+xForwardShift+xSideShift+arrowLengthInLine*lineCos-arrowLengthInLine*lineCos/i;
+            lastPositionY = y2+yForwardShift-ySideShift+arrowLengthInLine*lineSin-arrowLengthInLine*lineSin/i;
+            ctx.lineTo(lastPositionX, lastPositionY);
             ctx.stroke();
         }
     }
@@ -85,32 +97,33 @@ const createEllipse = (x, y, number) => {
     ctx.ellipse(x, y, 50, 10, Math.PI*number/5,
         -Math.PI/2+extraAngle, Math.PI/2-extraAngle-0.05*lineWidth*2);
     ctx.stroke();
-    for(let i = 0; lineWidth*3-i > 0; i++) {
+    for(let i = arrowWidth; i > 0; i--) {
         ctx.beginPath();
-        const currentWidth = lineWidth*3-i;
-        ctx.lineWidth = currentWidth;
+        ctx.lineWidth = i;
         ctx.ellipse(x, y, 50, 10, Math.PI*number/5,
-            Math.PI/2 - extraAngle - 0.1*(currentWidth), Math.PI/2 - extraAngle - 0.1*(currentWidth-1));
+            Math.PI/2 - extraAngle - arrowLengthInElipse*(i)/(64*arrowWidth),
+            Math.PI/2 - extraAngle - arrowLengthInElipse*(i-1)/(64*arrowWidth));
         ctx.stroke();
     }
     ctx.lineWidth = lineWidth;
 }
-const vertixes = [createVertixes(10, radius, window.innerWidth/4),
-    createVertixes(10, radius, window.innerWidth*3/4)];
+const vertixes = [createVertixes(countOfVertixes, radius, window.innerWidth/4),
+    createVertixes(countOfVertixes, radius, window.innerWidth*3/4)];
 const createGraphs = () => {
     for(let i = 0; i < textAreas.length; i++) {
         const circleOfVertixes = vertixes[i];
         const textArea = textAreas[i];
-        for(let row = 0; row < size; row++) {
+        for(let row = 0; row < countOfVertixes; row++) {
             matrix[row] = [];
-            textArea.textContent += `${row+1}. `;
-            if(row < 9) textArea.textContent += ' '
-            const columnsToLoop = i ? size : row+1;
+            const columnsToLoop = i ? countOfVertixes : row+1;
             for(let column = 0; column < columnsToLoop; column++) {
-                const number = Math.random()*2
-                const k = 0.745;
+                const number = Math.random()*2;
                 const resultNumber = number * k;
-                matrix[row][column] = resultNumber < 1 ? 0 : 1;
+                const oneOrZero = resultNumber < 1 ? 0 : 1;
+                matrix[row][column] = oneOrZero;
+                if(!i) {
+                    matrix[column][row] = oneOrZero;
+                }
                 if(matrix[row][column] === 1) {
                     if(row === column) {
                         createEllipse(circleOfVertixes[row].offsetLeft+vertixRadius,
@@ -124,12 +137,19 @@ const createGraphs = () => {
                             i);
                     }
                 }
+            }
+        }
+        for(let row = 0; row < countOfVertixes; row++) {
+            textArea.textContent += `${row+1}. `;
+            if(row < 9) textArea.textContent += ' '
+            for(let column = 0; column < countOfVertixes; column++) {
+                if(column > 9) textArea.textContent += ' '
                 textArea.textContent += `${matrix[row][column]} `;
             }
             textArea.textContent += `\n`;
         }
         textArea.textContent += '    '
-        for(let column = 1; column <= size; column++) {
+        for(let column = 1; column <= countOfVertixes; column++) {
             textArea.textContent += `${column} `
         }
     }
